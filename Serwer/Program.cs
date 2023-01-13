46ca458454fa4eea11549da3acdf352d7591cae2
TheMultii <34040020+TheMultii@users.noreply.github.com>
2023-01-13 13:46:09 +0100

    swagger config

--- Serwer/Program.cs
+++ Serwer/Program.cs
@@ -1,3 +1,15 @@
+using Microsoft.OpenApi.Models;
+const string baseAPIVersion = "v1";
+const string baseAPIName = "Serwis Rowerowy";
+
+string addZero(int x) => x < 10 ? $"0{x}" : x.ToString();
+
+string getAPIVersion() {
+    DateTime dateTime = DateTime.Now;
+    string monthDate = $"{addZero(dateTime.Month)}{addZero(dateTime.Day)}";
+    return $"{baseAPIVersion}-{monthDate}";
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,14 +17,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc(baseAPIVersion, new OpenApiInfo {
+        Version = getAPIVersion(),
+        Title = baseAPIName,
+        Description = "An ASP.NET Core Web API for managing bicycle service",
+        License = new OpenApiLicense {
+            Name = "MIT",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", baseAPIName);
+        c.RoutePrefix = "docs";
+        c.DocumentTitle = $"{baseAPIName} | Docs";
+    });
 }
 
 app.UseHttpsRedirection();

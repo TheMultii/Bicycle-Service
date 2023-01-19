9d911d5537bc27c9fb5d72921368e68efe4546f3
TheMultii <34040020+TheMultii@users.noreply.github.com>
2023-01-19 22:57:28 +0100

    Create UIDToDateTimeConverter.cs

---
+++ Klient/Helpers/UIDToDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using IdGen;
+using Microsoft.UI.Xaml.Data;
+
+namespace Klient.Helpers;
+
+public class UIDToDateTimeConverter : IValueConverter {
+
+    private static readonly DateTime epoch = new(2015, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    private static readonly IdStructure structure = new(41, 10, 12);
+    private static readonly IdGeneratorOptions options = new(structure, new DefaultTimeSource(epoch));
+    private static readonly IdGenerator generator = new(0, options);
+
+    public object Convert(object value, Type targetType, object parameter, string language) {
+        if (value is long uid) {
+            Id id = generator.FromId(uid);
+            return id.DateTimeOffset.ToLocalTime().DateTime;
+        }
+
+        throw new ArgumentException("");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+        throw new NotImplementedException();
+    }
+}

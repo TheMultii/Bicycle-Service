426a9483320bc46f9634446687f6d5016f90a359
TheMultii <34040020+TheMultii@users.noreply.github.com>
2023-01-19 16:57:01 +0100

    Price of a bicycle

---
+++ Klient/Helpers/DoubleToPriceStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System.Globalization;
+
+namespace Klient.Helpers;
+
+public class DoubleToPriceStringConverter : IValueConverter {
+
+    public object Convert(object value, Type targetType, object parameter, string language) {
+        if (value is double d) {
+            CultureInfo pl = new("pl-PL");
+            return $"W sumie do zapłaty: {d.ToString("C2", pl)}";
+        }
+
+        throw new ArgumentException("");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+        throw new NotImplementedException();
+    }
+}

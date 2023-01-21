6a01b67c978fdc206fbde3ac2eb1fba4e9e62b24
TheMultii <34040020+TheMultii@users.noreply.github.com>
2023-01-21 19:10:54 +0100

    DisplayError class

---
+++ Klient/Helpers/DisplayError.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klient.Helpers {
+    public static class DisplayError {
+        public static async Task<ContentDialogResult> show(object sender, string errorMessage, string errorTitle = "Błąd", string errorButtonText = "OK") {
+            ContentDialog dialog = new() {
+                XamlRoot = ((Control)sender).XamlRoot,
+                Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+                Title = errorTitle,
+                Content = errorMessage,
+                PrimaryButtonText = errorButtonText,
+                DefaultButton = ContentDialogButton.Primary
+            };
+
+            return await dialog.ShowAsync();
+        }
+    }
+}

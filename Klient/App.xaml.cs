0d26c22519c824958057d0d5c248e5258d6aed84
TheMultii <34040020+TheMultii@users.noreply.github.com>
2023-01-22 10:03:27 +0100

    Refresh button in orders management page
    removed contentgridpage

--- Klient/App.xaml.cs
+++ Klient/App.xaml.cs
@@ -8,31 +8,20 @@ using Klient.Notifications;
 using Klient.Services;
 using Klient.ViewModels;
 using Klient.Views;
-
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
 
 namespace Klient;
 
-// To learn more about WinUI 3, see https://docs.microsoft.com/windows/apps/winui/winui3/.
-public partial class App : Application
-{
-    // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
-    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
-    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
-    // https://docs.microsoft.com/dotnet/core/extensions/configuration
-    // https://docs.microsoft.com/dotnet/core/extensions/logging
-    public IHost Host
-    {
+public partial class App : Application {
+    public IHost Host {
         get;
     }
 
     public static T GetService<T>()
-        where T : class
-    {
-        if ((Current as App)!.Host.Services.GetService(typeof(T)) is not T service)
-        {
+        where T : class {
+        if ((Current as App)!.Host.Services.GetService(typeof(T)) is not T service) {
             throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs.");
         }
 
@@ -41,15 +30,13 @@ public partial class App : Application
 
     public static WindowEx MainWindow { get; } = new MainWindow();
 
-    public App()
-    {
+    public App() {
         InitializeComponent();
 
         Host = Microsoft.Extensions.Hosting.Host.
         CreateDefaultBuilder().
         UseContentRoot(AppContext.BaseDirectory).
-        ConfigureServices((context, services) =>
-        {
+        ConfigureServices((context, services) => {
             // Default Activation Handler
             services.AddTransient<ActivationHandler<LaunchActivatedEventArgs>, DefaultActivationHandler>();
 
@@ -68,7 +55,6 @@ public partial class App : Application
             services.AddSingleton<INavigationService, NavigationService>();
 
             // Core Services
-            services.AddSingleton<ISampleDataService, SampleDataService>();
             services.AddSingleton<IFileService, FileService>();
 
             // Views and ViewModels
@@ -78,10 +64,6 @@ public partial class App : Application
             services.AddTransient<DataGridPage>();
             services.AddTransient<WebViewViewModel>();
             services.AddTransient<WebViewPage>();
-            services.AddTransient<ContentGridDetailViewModel>();
-            services.AddTransient<ContentGridDetailPage>();
-            services.AddTransient<ContentGridViewModel>();
-            services.AddTransient<ContentGridPage>();
             services.AddTransient<ListDetailsViewModel>();
             services.AddTransient<ListDetailsPage>();
             services.AddTransient<MainViewModel>();
@@ -99,14 +81,11 @@ public partial class App : Application
         UnhandledException += App_UnhandledException;
     }
 
-    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
-    {
-        // TODO: Log and handle exceptions as appropriate.
-        // https://docs.microsoft.com/windows/windows-app-sdk/api/winrt/microsoft.ui.xaml.application.unhandledexception.
+    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e) {
+
     }
 
-    protected async override void OnLaunched(LaunchActivatedEventArgs args)
-    {
+    protected async override void OnLaunched(LaunchActivatedEventArgs args) {
         base.OnLaunched(args);
 
         GetService<IAppNotificationService>().Show(string.Format("AppNotificationSamplePayload".GetLocalized(), AppContext.BaseDirectory, "AppDescription".GetLocalized(), "Włączono aplikację"));
